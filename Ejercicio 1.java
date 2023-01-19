484f42d05dd2a3e6d76b09a61f0ab130d0a6fc97
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-19 18:08:36 -0300

    Updated

---
+++ Ejercicio 1.java
@@ -0,0 +1,25 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        // Declaración de variables
+
+        int num1 = 7, num2 = 2, num3 = 6, suma = 0;
+
+        // Muestra el contenido de las variables
+
+        System.out.println("Numero1 = " + num1);
+
+        System.out.println("Numero2 = " + num2);
+
+        System.out.println("Numero3 = " + num3);
+
+        // Se hace la suma
+
+        suma = num1 + num2 + num3;
+
+        System.out.println("Resultado = " + suma);
+
+    }
+
+}
