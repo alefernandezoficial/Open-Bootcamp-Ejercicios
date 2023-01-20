9e408e02a03f9f27c6485ea7c8f3b238bc47bc7a
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-20 15:00:50 -0300

    Updated

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,15 +3,11 @@ class Main {
     public static void main(String[] args) {
 
         int num1 = 7, num2 = 2, num3 = 6, suma = 0;
-
         System.out.println("Numero1 = " + num1);
-
         System.out.println("Numero2 = " + num2);
-
         System.out.println("Numero3 = " + num3);
 
         suma = num1 + num2 + num3;
-
         System.out.println("Resultado = " + suma);
 
     }
