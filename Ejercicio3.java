086854974497794e1234cafd12516fc3ba2cd1cb
alefernandezoficial <alefernandezoficial@gmail.com>
2023-01-21 13:55:59 -0300

    Updated

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,48 +1,43 @@
-public class Ejercicio3 {
-
-    public static void main(String[] args) {
-        int numeroIf = 0;
-        if (numeroIf<0){
-            System.out.println("variable numeroIf " + numeroIf + " es negativo");
-        }
-        else if(numeroIf>0){
-            System.out.println("variable numeroIf " + numeroIf + " es positivo");
-        } else {
-            System.out.println("variable numeroIf es 0");
-        }
-
-        int numeroWhile = 1;
-        while(numeroWhile < 3){
-            numeroWhile++;
-            System.out.println("variable numeroWhile ahora: " + numeroWhile);
-        }
-
-        int numeroDoWhile = 0;
-        do{
-            numeroDoWhile++;
-            System.out.println("variable numeroDoWhile ahora: " + numeroDoWhile);
-        }while(numeroDoWhile < 3);
-
-        for(int numeroFor = 0; numeroFor <= 5; numeroFor++){
-            System.out.println("variable numeroFor ahora: " + numeroFor);
-        }
-
-        String estacion = "otoño";
-        switch(estacion) {
-            case "verano":
-                System.out.println("Es verano");
-                break;
-            case "invierno":
-                System.out.println("Es invierno");
-                break;
-            case "primavera":
-                System.out.println("Es primavera");
-                break;
-            case "otoño":
-                System.out.println("Es otoño");
-                break;
-            default:
-                System.out.println("No es una estación");
-        }
+public class Ejercicio3{
+public static void main(String[] args) {
+    int numeroIf = 0;
+    if (numeroIf < 0){
+        System.out.println("variable numeroIf " + numeroIf + " es negativo");
     }
+    else if (numeroIf > 0){
+        System.out.println("variable numero If " + numeroIf + " es positivo");
+    } else {
+        System.out.println("variable numeroIf es 0");
+    }
+
+    int numeroWhile = 1;
+    while(numeroWhile < 3){
+        numeroWhile++;
+        System.out.println("variable numeroWhile ahora: " + numeroWhile);
+    }
+
+    int numeroDoWhile = 0;
+    do{
+        numeroDoWhile++;
+        System.out.println("variable numeroDoWhile ahora: " + numeroDoWhile);
+    }while(numeroDoWhile < 3);
+
+    String estacion = "otoño";
+    switch(estacion){
+        case "verano":
+            System.out.println("Es verano");
+        break;
+        case "invierno":
+            System.out.println("Es invierno");
+        break;
+        case "primavera":
+            System.out.println("Es primavera");
+        break;
+        case "otoño":
+            System.out.println("Es otoño");
+        break;
+        default:
+            System.out.println("No es una estacion del año");
+    }
+}
 }
